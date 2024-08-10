e1d10ae845515952c93130d09f39b3f74a2c0aec
João Vitor Pereira <joaovitorpessoal2@gmail.com>
2024-08-10 08:49:39 -0300

    add theme config using gluestack eject config

--- App.tsx
+++ App.tsx
@@ -1,5 +1,6 @@
 import { Roboto_400Regular, Roboto_700Bold } from "@expo-google-fonts/roboto";
 import { useFonts } from "@expo-google-fonts/roboto/useFonts";
+import { config } from "./config/gluestack-ui.config"; //using config eject by gluestack
 import { GluestackUIProvider, Text } from "@gluestack-ui/themed";
 import { StatusBar, View } from "react-native";
 
@@ -7,7 +8,7 @@ export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_700Bold, Roboto_400Regular });
 
   return (
-    <GluestackUIProvider>
+    <GluestackUIProvider config={config}>
       <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
         <StatusBar
           barStyle={"light-content"}
