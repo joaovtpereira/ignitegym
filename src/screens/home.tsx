0bc2c3a842423d78d3a512e4fc4d5ecbc4416aee
João Vitor Pereira <joaovitorpessoal2@gmail.com>
2024-08-10 16:56:06 -0300

    add header list exercises

--- src/screens/home.tsx
+++ src/screens/home.tsx
@@ -1,6 +1,6 @@
 import { Group } from "@components/group";
 import { HomeHeader } from "@components/homeHeader";
-import { Center, HStack, Text, VStack } from "@gluestack-ui/themed";
+import { Center, Heading, HStack, Text, VStack } from "@gluestack-ui/themed";
 import { useState } from "react";
 import { FlatList } from "react-native";
 
@@ -12,27 +12,41 @@ export function Home() {
     <VStack flex={1}>
       <HomeHeader />
 
-      <HStack>
-        <FlatList
-          data={groups}
-          keyExtractor={(group) => group}
-          renderItem={({ item }) => (
-            <Group
-              text={item}
-              isActive={groupSelected === item}
-              onPress={() => setGroupSelected(item)}
-            />
-          )}
-          horizontal
-          showsHorizontalScrollIndicator={false}
-          contentContainerStyle={{ paddingHorizontal: 32 }}
-          style={{
-            marginVertical: 40,
-            maxHeight: 44,
-            minHeight: 44,
-          }}
-        />
-      </HStack>
+      <FlatList
+        data={groups}
+        keyExtractor={(group) => group}
+        renderItem={({ item }) => (
+          <Group
+            text={item}
+            isActive={groupSelected === item}
+            onPress={() => setGroupSelected(item)}
+          />
+        )}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        contentContainerStyle={{ paddingHorizontal: 32 }}
+        style={{
+          marginVertical: 40,
+          maxHeight: 44,
+          minHeight: 44,
+        }}
+      />
+
+      <VStack paddingHorizontal="$8">
+        <HStack
+          justifyContent="space-between"
+          marginBottom={"$5"}
+          alignItems="center"
+        >
+          <Heading color="$gray200" fontSize={"$md"} fontFamily="$heading">
+            Exercícios
+          </Heading>
+
+          <Text color="$gray200" fontSize={"$sm"} fontFamily="$body">
+            4
+          </Text>
+        </HStack>
+      </VStack>
     </VStack>
   );
 }
