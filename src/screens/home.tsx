c45a9c25151c92a7b9f1eb4f14c9ab279e15fc51
João Vitor Pereira <joaovitorpessoal2@gmail.com>
2024-08-10 16:52:27 -0300

    add flatlist horizontal do render groups

--- src/screens/home.tsx
+++ src/screens/home.tsx
@@ -2,28 +2,35 @@ import { Group } from "@components/group";
 import { HomeHeader } from "@components/homeHeader";
 import { Center, HStack, Text, VStack } from "@gluestack-ui/themed";
 import { useState } from "react";
+import { FlatList } from "react-native";
 
 export function Home() {
+  const [groups, setGroups] = useState(["costa", "triceps", "peito"]);
   const [groupSelected, setGroupSelected] = useState("costa");
+
   return (
     <VStack flex={1}>
       <HomeHeader />
 
       <HStack>
-        <Group
-          text="costa"
-          isActive={groupSelected === "costa"}
-          onPress={() => setGroupSelected("costa")}
-        />
-        <Group
-          text="triceps"
-          isActive={groupSelected === "triceps"}
-          onPress={() => setGroupSelected("triceps")}
-        />
-        <Group
-          text="peito"
-          isActive={groupSelected === "peito"}
-          onPress={() => setGroupSelected("peito")}
+        <FlatList
+          data={groups}
+          keyExtractor={(group) => group}
+          renderItem={({ item }) => (
+            <Group
+              text={item}
+              isActive={groupSelected === item}
+              onPress={() => setGroupSelected(item)}
+            />
+          )}
+          horizontal
+          showsHorizontalScrollIndicator={false}
+          contentContainerStyle={{ paddingHorizontal: 32 }}
+          style={{
+            marginVertical: 40,
+            maxHeight: 44,
+            minHeight: 44,
+          }}
         />
       </HStack>
     </VStack>
