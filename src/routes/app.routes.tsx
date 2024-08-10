36ff3101981d2343d28cc172ecaedc245e4be047
João Vitor Pereira <joaovitorpessoal2@gmail.com>
2024-08-10 10:39:38 -0300

    add app routes navigation

---
+++ src/routes/app.routes.tsx
@@ -0,0 +1,32 @@
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { NativeStackNavigationProp } from "@react-navigation/native-stack";
+import { Exercise } from "@screens/exercise";
+import { History } from "@screens/history";
+import { Home } from "@screens/home";
+import { Profile } from "@screens/profile";
+
+type AuthRoutes = {
+  home: undefined;
+  history: undefined;
+  profile: undefined;
+  exercise: undefined;
+};
+
+// export type AuppNavigateRoutesProps = NativeStackNavigationProp<AuthRoutes>;
+
+const { Navigator, Screen } = createBottomTabNavigator<AuthRoutes>();
+
+export function AppRoutes() {
+  return (
+    <Navigator
+      screenOptions={{
+        headerShown: false,
+      }}
+    >
+      <Screen name="home" component={Home} />
+      <Screen name="history" component={History} />
+      <Screen name="profile" component={Profile} />
+      <Screen name="exercise" component={Exercise} />
+    </Navigator>
+  );
+}
