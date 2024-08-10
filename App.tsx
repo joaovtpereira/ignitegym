64a1c2563852ee09ce445ca35e24db37cc29779f
João Vitor Pereira <joaovitorpessoal2@gmail.com>
2024-08-10 09:16:15 -0300

    move loading folder componet | remove imports

--- App.tsx
+++ App.tsx
@@ -1,10 +1,10 @@
+import { Loading } from "@components/loading";
 import { Roboto_400Regular, Roboto_700Bold } from "@expo-google-fonts/roboto";
 import { useFonts } from "@expo-google-fonts/roboto/useFonts";
-import { config } from "./config/gluestack-ui.config"; //using config eject by gluestack
-import { Center, GluestackUIProvider, Text } from "@gluestack-ui/themed";
-import { StatusBar, View } from "react-native";
-import { Loading } from "@components/Loading";
+import { GluestackUIProvider } from "@gluestack-ui/themed";
 import { SignIn } from "@screens/signIn";
+import { StatusBar } from "react-native";
+import { config } from "./config/gluestack-ui.config"; //using config eject by gluestack
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_700Bold, Roboto_400Regular });
