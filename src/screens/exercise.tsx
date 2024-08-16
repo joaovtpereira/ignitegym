88b6d5e4115e8c8afcaa9c745cec4c80977de9aa
João Vitor Pereira <joaovitorpessoal2@gmail.com>
2024-08-15 21:27:11 -0300

    add box to check exercise is done

--- src/screens/exercise.tsx
+++ src/screens/exercise.tsx
@@ -6,12 +6,16 @@ import {
   Text,
   VStack,
   Image,
+  Box,
 } from "@gluestack-ui/themed";
 import { useNavigation } from "@react-navigation/native";
 import { AuppNavigateRoutesProps } from "@routes/app.routes";
 import { ArrowLeft } from "lucide-react-native";
-import { TouchableOpacity } from "react-native";
+import { ScrollView, TouchableOpacity } from "react-native";
 import BodySvg from "@assets/body.svg";
+import SeriesSvg from "@assets/series.svg";
+import RepetitionSvg from "@assets/repetitions.svg";
+import { Button } from "@components/button";
 
 export function Exercise() {
   const navigation = useNavigation<AuppNavigateRoutesProps>();
@@ -48,19 +52,53 @@ export function Exercise() {
         </HStack>
       </VStack>
 
-      <VStack padding={"$8"}>
-        <Image
-          source={{
-            uri: "https://s2-oglobo.glbimg.com/WyvC_yuCqntB5JjnFP1cTdrVvpk=/0x0:1339x1181/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_da025474c0c44edd99332dddb09cabe8/internal_photos/bs/2022/0/j/2NMxiNQ7aFEpXwjk5aCA/exercicio004-copia.jpg",
-          }}
-          alt="Exercicio detalhado"
-          marginBottom={"$3"}
-          resizeMode="cover"
-          rounded={"$lg"}
-          w={"$full"}
-          h={"$80"}
-        />
-      </VStack>
+      <ScrollView
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{ paddingBottom: 62 }}
+      >
+        <VStack padding={"$8"}>
+          <Image
+            source={{
+              uri: "https://s2-oglobo.glbimg.com/WyvC_yuCqntB5JjnFP1cTdrVvpk=/0x0:1339x1181/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_da025474c0c44edd99332dddb09cabe8/internal_photos/bs/2022/0/j/2NMxiNQ7aFEpXwjk5aCA/exercicio004-copia.jpg",
+            }}
+            alt="Exercicio detalhado"
+            marginBottom={"$3"}
+            resizeMode="cover"
+            rounded={"$lg"}
+            w={"$full"}
+            h={"$80"}
+          />
+
+          <Box
+            bg="$gray600"
+            rounded={"$md"}
+            paddingBottom={"$4"}
+            paddingHorizontal={"$4"}
+          >
+            <HStack
+              alignItems="center"
+              justifyContent="space-around"
+              marginBottom={"$6"}
+              marginTop={"$5"}
+            >
+              <HStack alignItems="center">
+                <SeriesSvg />
+                <Text color="$gray200" marginLeft={"$2"}>
+                  3 séries
+                </Text>
+              </HStack>
+              <HStack alignItems="center">
+                <RepetitionSvg />
+                <Text color="$gray200" marginLeft={"$2"}>
+                  12 repetições
+                </Text>
+              </HStack>
+            </HStack>
+
+            <Button title="Realizado" />
+          </Box>
+        </VStack>
+      </ScrollView>
     </VStack>
   );
 }
