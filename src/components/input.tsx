711b8d5979efff2c0846659a48ffe5a132adf790
João Vitor Pereira <joaovitorpessoal2@gmail.com>
2024-08-17 11:24:01 -0300

    error message

--- src/components/input.tsx
+++ src/components/input.tsx
@@ -1,31 +1,53 @@
-import { Input as GlueStackInput, InputField } from "@gluestack-ui/themed";
+import {
+  FormControl,
+  FormControlError,
+  FormControlErrorText,
+  Input as GlueStackInput,
+  InputField,
+} from "@gluestack-ui/themed";
 import { ComponentProps } from "react";
 
 type Props = ComponentProps<typeof InputField> & {
   isReadOnly?: boolean;
+  errorMensage?: string | null;
+  isInvalid?: boolean;
 };
 
-export function Input({ isReadOnly = false, ...rest }: Props) {
+export function Input({
+  isReadOnly = false,
+  errorMensage = null,
+  isInvalid = false,
+  ...rest
+}: Props) {
+  const invalid = !!errorMensage || isInvalid;
   return (
-    <GlueStackInput
-      h={"$14"}
-      borderWidth={"$0"}
-      borderRadius={"$md"}
-      $focus={{
-        borderWidth: 1,
-        borderColor: "$green500",
-      }}
-      isReadOnly={isReadOnly}
-      opacity={isReadOnly ? 0.5 : 1}
-    >
-      <InputField
-        bg={"$gray700"}
-        px={"$4"}
-        color="$white"
-        fontFamily="$body"
-        placeholderTextColor={"$gray300"}
-        {...rest}
-      />
-    </GlueStackInput>
+    <FormControl isInvalid={invalid} w={"$full"} marginBottom={"$4"}>
+      <GlueStackInput
+        h={"$14"}
+        borderWidth={"$0"}
+        borderRadius={"$md"}
+        $focus={{
+          borderWidth: 1,
+          borderColor: "$green500",
+        }}
+        isReadOnly={isReadOnly}
+        opacity={isReadOnly ? 0.5 : 1}
+      >
+        <InputField
+          bg={"$gray700"}
+          px={"$4"}
+          color="$white"
+          fontFamily="$body"
+          placeholderTextColor={"$gray300"}
+          {...rest}
+        />
+      </GlueStackInput>
+
+      <FormControlError>
+        <FormControlErrorText color="$red500">
+          {errorMensage}
+        </FormControlErrorText>
+      </FormControlError>
+    </FormControl>
   );
 }
--- src/screens/signUp.tsx
+++ src/screens/signUp.tsx
@@ -23,7 +23,11 @@ type FormDataProps = {
 
 export function SignUp() {
   const navigation = useNavigation();
-  const { control, handleSubmit } = useForm<FormDataProps>();
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormDataProps>();
 
   function handleGoBackSignInScreen() {
     navigation.goBack();
@@ -62,12 +66,16 @@ export function SignUp() {
             <Controller
               control={control}
               name="name"
+              rules={{
+                required: "Informe o nome",
+              }}
               render={({ field: { onChange, value } }) => (
                 <Input
                   placeholder="Nome"
                   secureTextEntry
                   onChangeText={onChange}
                   value={value}
+                  errorMensage={errors.name?.message}
                 />
               )}
             />
@@ -75,6 +83,13 @@ export function SignUp() {
             <Controller
               control={control}
               name="email"
+              rules={{
+                required: "informe o e-mail",
+                pattern: {
+                  value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                  message: "E-mail inválido",
+                },
+              }}
               render={({ field: { onChange, value } }) => (
                 <Input
                   placeholder="E-mail"
@@ -82,6 +97,7 @@ export function SignUp() {
                   autoCapitalize="none"
                   onChangeText={onChange}
                   value={value}
+                  errorMensage={errors.email?.message}
                 />
               )}
             />
