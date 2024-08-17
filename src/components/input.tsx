c5d0c741b934ecea58a734c6c1ef1a078e6c53a6
João Vitor Pereira <joaovitorpessoal2@gmail.com>
2024-08-17 11:26:19 -0300

    style error component

--- src/components/input.tsx
+++ src/components/input.tsx
@@ -23,15 +23,20 @@ export function Input({
   return (
     <FormControl isInvalid={invalid} w={"$full"} marginBottom={"$4"}>
       <GlueStackInput
+        isInvalid={invalid}
         h={"$14"}
         borderWidth={"$0"}
         borderRadius={"$md"}
         $focus={{
           borderWidth: 1,
-          borderColor: "$green500",
+          borderColor: invalid ? "$red500" : "$green500",
         }}
         isReadOnly={isReadOnly}
         opacity={isReadOnly ? 0.5 : 1}
+        $invalid={{
+          borderWidth: 1,
+          borderColor: "$red500",
+        }}
       >
         <InputField
           bg={"$gray700"}
